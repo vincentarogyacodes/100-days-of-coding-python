afb8e6bb26413b3ed71c5d87673dfff94290316b
VincentAROG <vincent.sick@gmail.com>
2025-08-29 23:57:22 +0530

    code shifts the indices of text

--- Beginner/ceasar_cipher.py
+++ Beginner/ceasar_cipher.py
@@ -1,14 +1,12 @@
 alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
+cipher = []
 text = input("Type your text: \n").lower()
 shift = int(input("Type the shift amount: \n"))
 
 list_of_text = list(text)
 
-def encrypt(original_text,shift_amount):
-    for i in alphabets:
-        if list_of_text in alphabets:
-            list_of_text[i] = shift
-
-encrypt(text,shift)
-print(list_of_text)
+# def encrypt(original_text,shift_amount):
+for i in text:
+    if i in alphabets:
+        cipher = alphabets.index(i) + shift
+        print(cipher)
