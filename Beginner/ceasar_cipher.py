7b1e1d832c8aa0b9e69f2f7bac836a45af659712
VincentAROG <vincent.sick@gmail.com>
2025-08-30 11:17:54 +0530

    Working Caesar Cipher program. Polish needed to handle validation issues.

--- Beginner/ceasar_cipher.py
+++ Beginner/ceasar_cipher.py
@@ -1,16 +1,29 @@
 alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-process = input("Type ENCRYPT to encrypt your message or type DECRYPT to decrypt received message: \n").lower()
-user_text = input("Type your text: \n").lower()
-shift = int(input("Type the shift amount: \n"))
+# process = input("Type ENCRYPT to encrypt your message or type DECRYPT to decrypt received message: \n").lower()
+# user_text = input("Type your text: \n").lower()
+# shift = int(input("Type the shift amount: \n"))
 
 def caesar(text, shifted_amount, direction):
     output_text = ""
-    if direction == "decode":
+    if direction == "decrypt":
         shifted_amount *= -1
+
     for letters in text:
+        if letters not in alphabets:
+            output_text += letters
+        else:
             shifted_position = alphabets.index(letters) + shifted_amount
             shifted_position %= len(alphabets)
             output_text += alphabets[shifted_position]
     print(output_text)
 
-caesar(text=user_text, shifted_amount=shift,direction=process)
+to_restart = True
+while to_restart:
+    process = input("Type ENCRYPT to encrypt your message or type DECRYPT to decrypt received message: \n").lower()
+    user_text = input("Type your text: \n").lower()
+    shift = int(input("Type the shift amount: \n"))
+    caesar(text=user_text, shifted_amount=shift,direction=process)
+    restart = input("Type yes if you want to go again? Or, type no \n").lower
+    if restart == "no":
+        to_restart = False
+        print("Goodbye")
