37f46fd62518d5e5ca650d4afc93a4962062b5ba
VincentAROG <vincent.sick@gmail.com>
2025-08-30 06:32:51 +0530

    Caesar Cipher program. Polish needed!

--- Beginner/ceasar_cipher.py
+++ Beginner/ceasar_cipher.py
@@ -1,12 +1,16 @@
 alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-cipher = []
-text = input("Type your text: \n").lower()
+process = input("Type ENCRYPT to encrypt your message or type DECRYPT to decrypt received message: \n").lower()
+user_text = input("Type your text: \n").lower()
 shift = int(input("Type the shift amount: \n"))
 
-list_of_text = list(text)
+def caesar(text, shifted_amount, direction):
+    output_text = ""
+    if direction == "decode":
+        shifted_amount *= -1
+    for letters in text:
+            shifted_position = alphabets.index(letters) + shifted_amount
+            shifted_position %= len(alphabets)
+            output_text += alphabets[shifted_position]
+    print(output_text)
 
-# def encrypt(original_text,shift_amount):
-for i in text:
-    if i in alphabets:
-        cipher = alphabets.index(i) + shift
-        print(cipher)
+caesar(text=user_text, shifted_amount=shift,direction=process)
